aa4380ab2fdd2eacbad1aa3b929fdb77435602ad
AlonsoSMdV <asanmor011004@gmail.com>
2022-12-11 18:15:22 +0100

    Bew Commit

---
+++ Ejercicio16.java
@@ -0,0 +1,10 @@
+import matematicas.Varias;
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        for(int i = 1; i < 99999; i++) {
+            if (matematicas.Varias.esCapicua(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
