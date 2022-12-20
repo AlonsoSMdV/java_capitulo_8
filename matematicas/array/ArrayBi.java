a598d73da10d4bd074236ec177ee586fbab8645d
AlonsoSMdV <asanmor011004@gmail.com>
2022-12-20 17:15:25 +0100

    New Commit

--- matematicas/array/ArrayBi.java
+++ matematicas/array/ArrayBi.java
@@ -5,16 +5,16 @@ import matematicas.Varias;
 public class ArrayBi {
         public static void muestraMatriz(int[][]a){
             int digitosMax = 0;
-                for(int j = 0; j < a.length; j++){
-                    for(int i = 0; i < a[i].length; i++){
+                for(int i = 0; i < a.length; i++){
+                    for(int j = 0; j < a[i].length; j++){
                         int digitos = Varias.digitos(a[i][j]);
                         if(digitos > digitosMax);
                         digitosMax = digitos;
                     }
                 }
             String format = "%"+digitosMax+"d";
-            for(int j = 0; j < a.length; j++){
-                for(int i = 0; i < a[i].length; i++){
+            for(int i = 0; i < a.length; i++){
+                for(int j = 0; j < a[i].length; j++){
                     System.out.printf(format,a[i][j]);
                 }
                 System.out.println();
@@ -32,8 +32,8 @@ public class ArrayBi {
      */
         public static int[][] generaArrayBiInt(int n, int m, int min, int max){
             int[][] a = new int[n][m];
-            for(int j = 0; j < n; j++){
-                for(int i = 0; i < n; i++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
                     a[i][j] = (int)(Math.random()*(max-min+1)*min);
                 
                 }
@@ -81,20 +81,13 @@ public class ArrayBi {
      * @return 
      */
             public static int[] coordenadasEnArrayBiInt(int[][] a, int v){
-                int[] ret = {-1, -1};
-                if(a != null || a.length > 0){
-                    for(int i = 0; i <a.length;i++){
-                        for(int j = 0; j<a.length;j++){
-                            if(a[i][j] == v){
-                                ret[0] = i;
-                                ret[1] = j;
-                                break;
-                            }
-                        }
-                    }
+                int[] ret = new int[a.length];
+                for (int f = 0; f < a.length; f++) {
+                    ret[f] = a[f][v];
                 }
                 return ret;
-            }
+                }
+                
     /*          5. esPuntoDeSilla             */
     /**
      * 
@@ -114,4 +107,43 @@ public class ArrayBi {
 
         }
     /*          6. diagonal            */
+    /**
+    * Devuelve un array que contiene una de las diagonales del array
+    * bidimensional que se pasa como parámetro.
+    *
+    * @param x array bidimiensional de números enteros
+    * @param fila fila del número que marcará las dos posibles
+    * diagonales dentro del array <code>x</code>
+    * @param columna columna del número que marcará las dos posibles
+    * diagonales dentro del array <code>x</code>
+    * @param direccion cadena de caracteres que indica cuál de las dos
+    * posibles diagonales se devolverá; la cadena <code>
+    * "nose"</code> indica que se elige la diagonal que
+    * va del noroeste hacia el sureste, mientras que la
+    * cadena <code>"neso"</code> indica que se elige la
+    * diagonal que va del noreste hacia el suroeste
+    * @return array unidimensional que contiene una diagonal
+    * definida por un número determinado por <code>fila
+    * </code> y <code>columna</code> y una dirección
+    * determinada por el parámetro <code>direccion
+    * </code>
+    */
+    public static int[] diagonal(int x[][], int fila, int columna, String direccion) {
+        int elementos = 0, i, j;
+        int[] diagonalAux = new int [1000];
+        for (i = 0; i < x.length; i++) {
+            for (j = 0; j < x[0].length; j++) {
+                if ( (((columna - j) == (fila - i)) && (direccion.equals("nose")))
+                || (((columna - j) == (i - fila)) && (direccion.equals("neso"))) ) {
+                    diagonalAux[elementos++] = x[i][j];
+                }
+            }
+        }
+        int[] diagonal = new int[elementos];
+        for (j = 0; j < elementos; j++) {
+            diagonal[j] = diagonalAux[j];
+        }
+        return diagonal;
+    }
 }
+
