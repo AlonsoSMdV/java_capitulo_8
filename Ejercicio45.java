36ac6a22816abd3b64f1f53d5343e5e1b522a809
AlonsoSMdV <asanmor011004@gmail.com>
2023-01-09 18:13:18 +0100

    New Commit

---
+++ Ejercicio45.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Ejercicio45 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca la altura de la figura: ");
+        int altura = sc.nextInt();
+        for (int i = 1; i < altura; i++) {
+            System.out.print(linea('*', i));
+            System.out.print(linea(' ', altura * 2 - i * 2 - 1));
+            System.out.println(linea('*', i));
+        }
+        System.out.println(linea('*', altura * 2 - 1));
+        sc.close();
+    }
+    public static String linea(char caracter, int repeticiones) {
+        String resultado = "";
+        for (int i = 0; i < repeticiones; i++) {
+            resultado += caracter;
+        }
+        return resultado;
+    }
+}
